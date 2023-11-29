d2c6f0c42d4660a2eaa7526ff03a4175020877b8
taijusanagi <taijusanagi@gmail.com>
2023-11-29 18:05:30 +0900

    add initial unity

---
+++ Deploy/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const path = require('path');
+const port = 8080;
+
+app.use(express.static(path.join(__dirname, '/')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, + '/index.html'));
+})
+
+http.createServer(app).listen(port, () => {
+    console.log(`Server running on localhost:${port}`);
+});
