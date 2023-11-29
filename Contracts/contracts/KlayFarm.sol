02becf9e0bd2b8b8249a2ca81d4422c01ba94a56
taijusanagi <taijusanagi@gmail.com>
2023-11-29 18:44:01 +0900

    add contract

---
+++ Contracts/contracts/KlayFarm.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract KlayFarm is ERC721 {
+    constructor() ERC721("KlayFarm", "KF") {}
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+}
