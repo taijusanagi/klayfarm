0abe1589ad3fe086cf51742341f414563fa8f68a
taijusanagi <taijusanagi@gmail.com>
2023-11-30 22:01:49 +0900

    try

--- App/index.js
+++ App/index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const app = express();
 const http = require("http");
-const path = require("path");
-const port = 8080;
+// const path = require("path");
+const port = 3000;
 
-app.use(express.static(path.join(__dirname, "/")));
+// app.use(express.static(path.join(__dirname, "/")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, +"/index.html"));
+  res.send("Express on Vercel");
 });
 
-http.createServer(app).listen(port, () => {
+app.listen(port, () => {
   console.log(`Server running on localhost:${port}`);
 });
