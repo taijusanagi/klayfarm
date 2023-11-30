55a8f94c5ba729f3a5eb35cc6aaf0fea70e9d02f
taijusanagi <taijusanagi@gmail.com>
2023-11-30 22:15:23 +0900

    try 6

--- App/index.js
+++ App/index.js
@@ -4,16 +4,15 @@ const app = express();
 const path = require("path");
 const port = 8080;
 
-app.use(express.static(path.join(process.cwd(), "/")));
+app.use(express.static("public"));
+// app.use(express.static(path.join(process.cwd(), "/")));
 
 // app.get("/", (req, res) => {
 //   res.sendFile(path.join(process.cwd(), +"/index.html"));
 // });
 
 app.get("/", (req, res) => {
-  console.log("__dirname");
-  console.log(__dirname);
-  res.sendFile("index.html", { root: __dirname });
+  res.sendFile("index.html", { root: path.join(__dirname, "public") });
 });
 
 app.listen(port, () => {
