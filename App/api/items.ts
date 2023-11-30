6eb3fdff741071590a9394f3f96f49983d8a12d1
taijusanagi <taijusanagi@gmail.com>
2023-11-30 23:08:53 +0900

    miminum api

--- App/api/items.ts
+++ App/api/items.ts
@@ -1,8 +1,15 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  res.status(200).json({
-    message: "Hello world",
-    cookies: req.cookies,
-  });
+import { ethers } from "ethers";
+const { abi } = require("./KlayFarm.json");
+const address = "0xCa12692fbF90F3551095f36BD2efcE9119330BBD";
+const rpc = "https://public-en-baobab.klaytn.net";
+const provider = new ethers.providers.JsonRpcProvider(rpc);
+const contract = new ethers.Contract(address, abi, provider);
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const { id } = req.query;
+  console.log(id);
+  const items = await contract.getAllItems(id);
+  res.status(200).json(items);
 }
